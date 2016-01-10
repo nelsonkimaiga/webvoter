db739ca3ea7f32e434018bc780bdec9e05f12f01
kimaiga <nelsonkimaiga@aol.com>
2016-01-10 19:57:22 +0300

    Rectified server issue

--- build/generated/src/org/apache/jsp/vote_jsp.java
+++ build/generated/src/org/apache/jsp/vote_jsp.java
@@ -5,7 +5,6 @@ import javax.servlet.http.*;
 import javax.servlet.jsp.*;
 import java.sql.*;
 import java.io.*;
-import javax.imageio.ImageIO;
 
 public final class vote_jsp extends org.apache.jasper.runtime.HttpJspBase
     implements org.apache.jasper.runtime.JspSourceDependent {
@@ -186,33 +185,9 @@ public final class vote_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("<p id=\"contact-error\" class=\"text-danger lead\"></p>\n");
       out.write("<div class=\"row\">\n");
       out.write("  <!-- column 1 president -->\n");
-      out.write("  <div class=\"col-md-3\">\n");
+      out.write("  <div class=\" col-lg-3 col-md-3 col-sm-3\">\n");
       out.write("    <span><h3>President</h3></span>\n");
-
-    String names = request.getParameter("president").toString();
-       String[] devide=names.split(" ") ;
-       String fn=devide[0];
-       String ln=devide[1];
-    
-Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-Connection imageconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uchaguzi","root","");
-String imagequery = "SELECT Image FROM candidates where Fname ='"+fn+"' and Lname ='"+ln+"'";
-
-Statement imagest = imageconn.createStatement();
-ResultSet imagers = imagest.executeQuery(imagequery);
-while(imagers.next())
-{    
-
-      out.write("\n");
-      out.write("    <img src=\"");
-Blob test=imagers.getBlob("image");
-      out.write("\" class=\"img-rounded\">\n");
-      out.write(" ");
-; 
-}
-
-      out.write("\n");
+      out.write("    <img src=\"\" class=\"img-rounded\">\n");
       out.write("    <br>\n");
       out.write("    <select class=\"form-control\" name=\"president\">\n");
       out.write("      <option value=\"default\" selected disabled>Select President</option>\n");
--- build/web/vote.jsp
+++ build/web/vote.jsp
@@ -6,7 +6,7 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8" import="java.sql.*"%>
 <%@page import="java.io.*"%>
-<%@page import="javax.imageio.ImageIO"%>
+<%--<%@page import="javax.imageio.ImageIO"%>--%>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -144,26 +144,7 @@ return xmlHttp;
   <!-- column 1 president -->
   <div class=" col-lg-3 col-md-3 col-sm-3">
     <span><h3>President</h3></span>
-<%
-    String names = request.getParameter("president").toString();
-       String[] devide=names.split(" ") ;
-       String fn=devide[0];
-       String ln=devide[1];
-    
-Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-Connection imageconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uchaguzi","root","");
-String imagequery = "SELECT Image FROM candidates where Fname ='"+fn+"' and Lname ='"+ln+"'";
-
-Statement imagest = imageconn.createStatement();
-ResultSet imagers = imagest.executeQuery(imagequery);
-while(imagers.next())
-{    
-%>
-    <img src="<%Blob test=imagers.getBlob("image");%>" class="img-rounded">
- <%; 
-}
-%>
+    <img src="" class="img-rounded">
     <br>
     <select class="form-control" name="president">
       <option value="default" selected disabled>Select President</option>
